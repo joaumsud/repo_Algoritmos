a8d83d4a5d0db8ab4ceaf7169f4385290cc48ccb
JoauMSuD <joaumsud@hotmail.com>
2021-03-23 07:32:30 -0300

    Arrumando os exercicios

--- fatorial.c
+++ fatorial.c
@@ -1,15 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <locale.h>
 int main()
 {
-    int n, fatorial = 1;
-    while(n > 0 ){
-        printf("Digite um numero: ");
-        scanf("%d",&n);
-        while (n > 1){
-            fatorial = fatorial * n;
-            n = n - 1;
-        }
-        printf("Valor do fatorial e: %d\n", fatorial );
+    setlocale(LC_ALL, "");
+    int numero, multiplicador, fatorial = 1;
+    printf("Digite um numero qualquer: ");
+    scanf("%d",&numero);
+    for(multiplicador =1; multiplicador <= numero; multiplicador++){
+        fatorial = multiplicador * fatorial;
     }
-
+    printf("Fatorial= %d\n", fatorial);
+    return(0);
 }
