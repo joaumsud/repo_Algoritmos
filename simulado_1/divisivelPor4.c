6fdcbfcb3a693064cf7526f4ba11dafedb1d8e74
JoauMSuD <joaumsud@hotmail.com>
2021-03-25 09:35:32 -0300

    numero divisivel

---
+++ simulado_1/divisivelPor4.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+
+int main(){
+    int i ;
+
+    for(i = 4; i <=100; i++){
+            if((i%4)==0){
+                printf("%d\n",i);
+            }
+    }      
+}
